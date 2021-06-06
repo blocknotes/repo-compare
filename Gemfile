402ecb49afa6ac8e3f4ac0b9116baa759387b083
Mattia Roccoberton <mat@blocknot.es>
2021-06-06 16:27:05 +0200

    Add some basic specs

--- Gemfile
+++ Gemfile
@@ -6,6 +6,8 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 gemspec
 
 group :development, :test do
+  gem 'rspec', '~> 3.10'
+
   # Tools
   gem 'pry'
 end
---
+++ spec/units/config_spec.rb
@@ -0,0 +1,56 @@
+# frozen_string_literal: true
+
+RSpec.describe RepoCompare::Config do
+  describe 'initialize' do
+    subject(:config_instance) { described_class.new }
+
+    context 'without a config file' do
+      before do
+        allow(File).to receive(:exist?).and_return(false)
+        allow(YAML).to receive(:load_file).and_return({})
+        config_instance
+      end
+
+      it 'loads the option from file' do
+        expect(YAML).not_to have_received(:load_file)
+      end
+    end
+
+    context 'with a config file' do
+      before do
+        allow(File).to receive(:exist?).and_return(true)
+        allow(YAML).to receive(:load_file).and_return({})
+        config_instance
+      end
+
+      it 'loads the option from file' do
+        expect(YAML).to have_received(:load_file)
+      end
+    end
+  end
+
+  describe '#to_h' do
+    subject(:to_h) { described_class.new.to_h }
+
+    it 'returns the initial config' do
+      expected_config = { 'ignore' => {}, 'paths' => [], 'source_branch' => 'master' }
+
+      expect(to_h).to eq expected_config
+    end
+  end
+
+  describe '#update' do
+    subject(:update) { described_class.new.update(results) }
+
+    let(:results) { {} }
+
+    before do
+      allow(File).to receive(:open)
+      update
+    end
+
+    it 'updates the config file' do
+      expect(File).to have_received(:open).with(described_class::CONFIG_PATH, 'w')
+    end
+  end
+end
