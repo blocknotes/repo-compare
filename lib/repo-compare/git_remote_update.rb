c728b250ef5829966af45c1043659f74a24f9a3d
Mattia Roccoberton <mat@blocknot.es>
2021-06-08 09:19:22 +0200

    Use git library to update the remote source

--- lib/repo-compare/git_remote_update.rb
+++ lib/repo-compare/git_remote_update.rb
@@ -1,20 +1,60 @@
 # frozen_string_literal: true
 
+require 'git'
+
 module RepoCompare
   # Update the remote source
   class GitRemoteUpdate
     def initialize(config)
-      @config = config
+      @config = config || {}
+      @error = nil
     end
 
-    def call
-      if `git remote`.split("\n").none? @config['source_name']
-        `git remote add -f #{@config['source_name']} #{@config['source_repo']}`
-        puts('error with git remote add') & exit if $?.exitstatus != 0
-      end
+    # Update the git remote source, if it is not presente it will be added
+    #
+    # - path: path of the local git repository (default: current working directory)
+    #
+    # Returns an hash with `success?` true when there are no errors.
+    # Otherwise returns an hash with `error`.
+    def call(path: Dir.pwd)
+      git = git_open(path)
+      return @error unless git
 
-      `git remote update > /dev/null 2>&1`
-      puts('error with git remote update') & exit if $?.exitstatus != 0
+      remote = git.remotes.find { |r| r.name == @config['source_name'] }
+      remote ||= git_remote_add(git)
+      return @error unless remote
+
+      return @error unless git_remote_update(remote)
+
+      { success?: true }
+    end
+
+    private
+
+    def fail(error = nil)
+      @error = { error: error }
+      nil
+    end
+
+    def git_open(path)
+      Git.open(path)
+    rescue ArgumentError => e
+      fail("git_open - #{e.inspect}")
+    end
+
+    def git_remote_add(git)
+      return fail('git_remote_add - Git index not writable') unless git.index.writable?
+
+      git.add_remote(@config['source_name'], @config['source_repo'])
+    rescue Git::GitExecuteError => e
+      fail("git_remote_add - #{e.inspect}")
+    end
+
+    def git_remote_update(remote)
+      remote.fetch
+      true
+    rescue Git::GitExecuteError => e
+      fail("git_remote_update - #{e.inspect}")
     end
   end
 end
